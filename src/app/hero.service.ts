44cf519affcb24ade3239836ec1147fcef690bc7
LeightonManningQA <lmanning@QA.com>
2021-03-16 16:45:28 +0000

    Added some more from the tour of heroes tutorial. Ready to get data from a server

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Hero} from './hero'
+import {HEROES} from './mock-heroes'
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  constructor(private messageService: MessageService) { }
+}
